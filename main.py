c82b2070fbfd9142a78c51d7068652542a206b70
TunaInABottle <social.tuna+github@pm.me>
2022-03-02 17:45:52 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from ticket_to_text import ticket_to_text
+
+
+img_path = "./02processedInput/test1.jpg"
+
+
+
+#@TODO iterate in folder path, write output in new folder
+
+ticket_to_text(img_path)
