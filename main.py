40dab84398ddba94c6e9eb63ca7859422d1ee611
TunaInABottle <social.tuna+github@pm.me>
2022-03-15 09:48:44 +0100

    Made venv, testing logging

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ def main():
     ##########################
     with open('./textFromImage/20220118_poli.jpg.txt', 'r') as file:
         process_ticket_text( file.read() )
-        # "Sconto UNISPESA -0,20\nFAG.CANNELL.PRIMIA *VI 1,9\nSconto Articolo -0,29\nSconto Articolo <0,29\nLENTICCHIE PRIMIA *VI 1,39\nSconto Articolo -0,40\nQuackers\n Articolo -0,40"
 
+
+        
 if __name__ == "__main__":
     main()
--- ticket_text_processor.py
+++ ticket_text_processor.py
@@ -1,28 +1,50 @@
 import re
+import pandas as pd
+import logging
 
 
-def process_ticket_text(ticket_string: str):
-    regex = re.compile(r"\S?\d*[.,]\d{2}$")
-    #regex = re.compile(r"-")
+def process_ticket_text(ticket_string: str) -> pd.DataFrame: 
 
+    # Identify string at the end of the line that have decimals numbers
+    ticket_entry_regex = re.compile(r"[\W\S]?\d+[.,]\d{2}$")
 
+    # Extract only strings that are receipt entries
     text_vect = ticket_string.splitlines()
-    #text_vect = ticket_string
+    newlist = list(filter(ticket_entry_regex.search, text_vect))
 
-    #print(text_vect)
-
-
-    #for i in text_vect:
-    #    if re.search(regex, i) is not None:
-    #        print(i)
-    newlist = list(filter(regex.search, text_vect))
-    print(newlist)
-
+    #Make new ticket entries
+    ticket_entries = []
     for idx, entry_string in enumerate(newlist):
-        format_ticket_entry(entry_string)
+        ticket_entries.append(format_ticket_entry(entry_string))
 
 
-    
-def format_ticket_entry(entry_string: str):
-    print(entry_string)
+    pd_ticket = pd.DataFrame(ticket_entries)
+
+    print(pd_ticket["price"].sum() - 2 * pd_ticket["price"].max() ) #@TODO throw warning if this is not 0
+
+    print(pd_ticket)
+
+    return pd_ticket
+
+
+def format_ticket_entry(entry_string: str) -> dict:
+    iva_regex = re.compile(r"\S?[Vv][Ii]")
+    clean_entry = re.sub( iva_regex , '', entry_string)
+
+    split_entry = clean_entry.split(' ')
+
+    price = split_entry[-1]
+
+    # if first character is not a number, guess it negative
+    price = re.sub(  r"^[^0-9]", '-', price)
+    # convert price to float
+    price = float( price.replace(",", ".") )
+
+
+    product = " ".join(split_entry[0:len(split_entry)-1])
+
+
+    entry_dict = {"name": product, "price": price}
+
+    return entry_dict
     # if sconto but not negative, put number as negative
