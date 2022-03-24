dd2aa5539bf297d9fb317885e06cb14d9c27d6fa
TunaInABottle <social.tuna+github@pm.me>
2022-03-24 18:58:46 +0100

    Testing new version with improved price detection

--- Entry.py
+++ Entry.py
@@ -1,8 +1,9 @@
 import re
 from typing import List
 
-
 class Entry:
+    __iva_col_regex = re.compile(r"\S?[Vv][Ii]")
+
     def __init__(self, entry_text: str):
         splitted_entry = entry_text.split(' ')
         self.product = self.extract_product(splitted_entry)
@@ -12,9 +13,12 @@ class Entry:
         return(self.product.ljust(25) + str(format(self.price, '.2f')).rjust(5, " "))
 
 
+    # TODO Make regex search for numbers, extract result, merge if space in between
     def extract_price(self, split_entry: List[str]) -> str:
         # Assumption: price is always the last element in an entry
         price = split_entry[-1]
+
+
         # if first character is not a number, guess it negative
         price = re.sub(  r"^[^0-9]", '-', price)
         
@@ -22,13 +26,14 @@ class Entry:
 
 
     def extract_product(self, split_entry: List[str]) -> str:
-        iva_col_regex = re.compile(r"\S?[Vv][Ii]")
         # Assumption: product is everything but the last element of an entry
         product = " ".join(split_entry[0:len(split_entry)-1]) 
         product = re.sub(  r"[ ]+$", '', product) #remove trailing space
-        product = re.sub( iva_col_regex , '', product) #remove column related to IVA
+        product = re.sub( self.__iva_col_regex , '', product) #remove column related to IVA
         return product
 
 
-    def IVA_entry(self) -> bool:
-        return re.search('cui IVA', self.product, re.IGNORECASE)
+    def is_relevant(self) -> bool:
+        if self.product == "":
+            return False
+        return not re.search('cui IVA|pagamento contante', self.product, re.IGNORECASE)
